a5862e11470540273dbf62c42031eb52bf2779bb
Leonardo Solis <leonardosolis111@gmail.com>
2025-11-06 00:40:40 -0600

    Add LittleLemon folder App

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Leonardo Solís on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
